baaa5ded69323f24927639706d479a4f46f4d9fe
mahi2222 <mahi.peruva@gmail.com>
2026-02-10 13:38:18 +0530

    url editted

--- marketing/urls.py
+++ marketing/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup.html', views.signup, name='signup'), # Keeping .html for backward compatibility with existing links
     path('signin.html', views.signin, name='signin'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('marketing/templates/marketing/dashboard/', views.dashboard, name='dashboard'),
     path('edit-profile/', views.edit_profile, name='edit_profile'),
     path('logout/', views.signout, name='signout'),
 ]
